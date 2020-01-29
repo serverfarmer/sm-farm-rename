812f338cbcf5d20bf90ee74ee7fe0fc02c4aed8c
Tomasz Klim <github@tomaszklim.pl>
2020-01-29 23:20:31 +0100

    use the new method of handling /usr/local/bin aliases

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 /opt/farm/scripts/setup/extension.sh sf-net-utils
-
-ln -sf /opt/farm/ext/farm-rename/rename-host.sh /usr/local/bin/rename-host
--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ -h /usr/local/bin/rename-host ]; then
-	rm -f /usr/local/bin/rename-host
-fi
